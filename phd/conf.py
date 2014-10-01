0f07e0ca8aca8b9b8e03ab8513234e819085d87e
claudiofahey <claudio.fahey@emc.com>
2014-10-01 14:26:16 -0700

    reorg

--- phd/conf.py
+++ phd/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 if on_rtd:
 	master_doc = 'index'
 else:
-	master_doc = 'phd/index'
+	master_doc = '%s/index' % hsk_dst
 
 # General information about the project.
 project = u'EMC Isilon Hadoop Starter Kit for %s' % hsk_distribution
@@ -200,13 +200,13 @@ latex_documents = [
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-latex_use_parts = False
+latex_use_parts = True
 
 # If true, show page references after internal links.
-latex_show_pagerefs = False
+latex_show_pagerefs = True
 
 # If true, show URL addresses after external links.
-latex_show_urls = False
+latex_show_urls = True
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
